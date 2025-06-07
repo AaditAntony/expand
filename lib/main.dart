3951b338b34325a080149ecacf72c82566371789
Aadit Antony <aaditantony1112@gmail.com>
2025-06-07 16:02:31 +0530

    format change

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,11 @@
-import 'package:expand/features/auth/view/login_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'invoice_form_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const ProviderScope(child: MyApp()));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Google Sign-In Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
+void main() {
+  runApp(
+    const MaterialApp(
+      home: InvoiceFormScreen(),
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
-    );
-  }
+    ),
+  );
 }
